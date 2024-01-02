940cdde80bb080ebd3a9e64f82e17b141037ed6c
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-01-02 12:59:18 -0800

    base-shell

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+/*
+ * File: 10-check_cycle.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if a singly-linked list contains a cycle.
+ * @list: A singly-linked list.
+ *
+ * Return: If there is no cycle - 0.
+ *         If there is a cycle - 1.
+ */
+int check_cycle(listint_t *list)
+	listint_t *slow = list, *fast = list;
+
+	while (fast && fast->next)
+{
+	slow = slow->next;
+	fast = fast->next->next;
+	if (slow == fast)
+		return (1);
+}
+	return (0);
+}
