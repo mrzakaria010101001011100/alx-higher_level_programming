e22edb3c4ddee87979eaad48ab06bb85c4f32f84
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-01-04 13:37:57 -0800

    base-shell

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
+    sum = 0
+    for i in range(len(sys.argv) - 1):
+        sum
             result += int(arg)
     print(result)
