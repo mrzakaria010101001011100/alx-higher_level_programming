aa2c87d1a529a5342d703ff68ffbbf08f1232efc
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-01-08 13:51:44 -0800

    base-shell

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+
     new_list = []
     for number in my_list:
         if (number % 2) == 0:
--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx >= 0 and idx < len(my_list):
-        dev my_list[idx]
-    return my_list
+
+    length_list = len(my_list) - 1
+    if idx < 0 or idx > length_list:
+        return(my_list)
+    else:
+        del my_list[idx]
+        return(my_list)
