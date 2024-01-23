50524cade39aad340ff6eda1a004eb2707bb4cb4
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-01-23 13:17:28 -0800

    base-shell

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -5,13 +5,16 @@ class Square:
         """Initialises the data"""
         self.size = size
         self.position = position
+
     def area(self):
         """Returns current square area"""
         return self.__size**2
+
     @property
     def size(self):
         """Getter method"""
         return self.__size
+
     @size.setter
     def size(self, value):
         """Setter method"""
@@ -20,6 +23,7 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+
     def my_print(self):
         """Prints the square"""
         if self.__size == 0:
@@ -33,6 +37,7 @@ class Square:
                 for j in range(self.__size):
                     print('#', end='')
                 print()
+
     @property
     def position(self):
         """Getter method"""
@@ -46,3 +51,17 @@ class Square:
         if any(type(i) != int for i in value) or any(j < 0 for j in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
+    def __str__(self):
+        """Same print behaviour as my_print"""
+        s = ""
+        if not self.__size:
+            return s
+        for y in range(self.__position[1]):
+            s += '\n'
+        for i in range(self.__size):
+            for x in range(self.__position[0]):
+                s += ' '
+            for j in range(self.__size):
+                s += '#'
+            s += '\n'
+        return s[: - 1]
