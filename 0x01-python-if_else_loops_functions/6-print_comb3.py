d6704ddfc3cc76a49d50cb29ddd8d7aa0ac90f50
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-01-31 14:47:30 -0800

    base-shell

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for x in range(i + 1, 10):
-        if i == 8 and x == 9:
-            print("{}{}".format(i, x))
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
         else:
-            print("{}{}".format(i, x), end=", ")
+            print("{}{}".format(digit1, digit2), end=", ")
