83bd7a20c9601c8ca58d50bc70feb33e7fe987c2
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-02-06 13:35:19 -0800

    base-shell

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""10. Student to JSON with filter"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        """initializes of the student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return a dictionary representation of a student instance
+        if attrs is a list of string, only attribute name contained
+        in this list must be retrieved.
+        otherwise, all attributes must be retrieved
+        """
+        try
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+            except Exception:
+                return self.__dict__
+            my_dict = dict()
+            for key, value in self.__dict__.items():
+                if ke in attrs:
+                    my_dict[key] = value
+            return my_dict
+    def reload_from_json(self, json):
+        """this function replaces all attributes of the student instance
+        withthe ones in the json argument
+        """
+        for  key, value in json.items():
+            if key in self.__dict__():
+                self.__dict__[key] = value
