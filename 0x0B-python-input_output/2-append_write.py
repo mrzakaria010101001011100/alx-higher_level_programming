af75d8735303717bc09045c3f6c65b5fa008061a
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-02-06 09:24:26 -0800

    base-shell

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defining write_file with two arguments"""
 
+
 def append_write(filename="", text=""):
     """ appends file with utf-8"""
     with open(filename, "a", encoding='utf-8') as f:
