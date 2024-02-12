2bfdf1ab4d2dd9c0e68707eb418799b2ce7e1db3
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-02-12 11:22:08 -0800

    base-shell

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-'''Module for Rectangle class.'''
+'''Module Rectangle class.'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''A Rectangle class.'''
+    '''Rectangle class'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        '''Constructor.'''
+        '''Constructor'''
         super().__init__(id)
         self.width = width
         self.height = height
@@ -16,7 +16,7 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        '''Width of this rectangle.'''
+        '''Width this rectangle.'''
         return self.__width
 
     @width.setter
@@ -26,7 +26,7 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        '''Height of this rectangle.'''
+        '''Height this rectangle.'''
         return self.__height
 
     @height.setter
@@ -36,7 +36,7 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        '''x of this rectangle.'''
+        '''x this rectangle.'''
         return self.__x
 
     @x.setter
@@ -46,7 +46,7 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        '''y of this rectangle.'''
+        '''y this rectangle.'''
         return self.__y
 
     @y.setter
