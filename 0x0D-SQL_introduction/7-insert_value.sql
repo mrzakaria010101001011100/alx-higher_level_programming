cea593e4f849af4b4742d43d3b2b2bfd037b42d7
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-03-12 15:40:13 -0700

    SQL5

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 --First-ADD inserts a new row in the table first_table TASK7.
-INSERT INTO first_table VALUES(89, 'Best School');
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
