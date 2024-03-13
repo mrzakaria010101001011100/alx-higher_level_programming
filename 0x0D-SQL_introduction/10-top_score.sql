cbc2c57d9853e122b6614a3075b2fa381a8eae01
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-03-12 17:34:44 -0700

    SQL12

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Lists all records of the table second_table.
+-- display both the score and the name.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- Updates the score  Bob to 10 in the table second_table TASK12.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
