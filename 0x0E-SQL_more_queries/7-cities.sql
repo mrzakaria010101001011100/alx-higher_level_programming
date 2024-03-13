c3e892248d9058fe7b9e82c200c912e84ea9a4fb
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-03-13 16:39:07 -0700

    MORE-SQL1

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) 
+-- id INT unique, auto generated, can’t be null and is a primary key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
+
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- Select all cities from the cities table where the state_id matches the id of Cal.
+-- Order the results in ascending order by the city id
+SELECT * FROM cities
+WHERE state_id = (
+    SELECT id FROM states WHERE name = 'California'
+)
+ORDER BY id ASC;
+
