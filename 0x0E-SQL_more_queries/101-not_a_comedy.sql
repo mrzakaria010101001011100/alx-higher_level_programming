c6a75ddcb5dbdf144ba6c7e8b123fd2b0846461f
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-03-13 22:41:51 -0700

    MORE-SQL16_Advanced

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- list all genres not linked to the show Dexter
+-- Each record should display:
+-- tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+
+SELECT name FROM tv_genres
+WHERE tv_genres.id NOT IN (
+      SELECT tv_genres.id FROM tv_genres
+      JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+      JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+      WHERE tv_shows.title = "DEXTER" )
+ORDER BY tv_genres.name;
