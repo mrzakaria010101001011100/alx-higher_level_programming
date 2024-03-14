5d7ca51fb0896d3643f74660afcbc2b685f3927d
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-03-13 17:26:12 -0700

    MORE-SQL1

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,10 +4,7 @@ SELECT tv_genres.genre AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_s
 FROM tv_genres
 -- Left join with tv_show_genres table to include genres with linked shows
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
--- Group by genre to count the number of shows linked to each genre
 GROUP BY tv_genres.genre
--- Filter out genres with no shows linked
 HAVING COUNT(tv_show_genres.tv_show_id) > 0
--- Order results by the number of shows linked in descending order
 ORDER BY number_of_shows DESC;
 
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,4 @@
 -- cript that creates the table unique_id on your MySQL server.
-
 CREATE TABLE IF NOT EXISTS 'unique_id' (
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
