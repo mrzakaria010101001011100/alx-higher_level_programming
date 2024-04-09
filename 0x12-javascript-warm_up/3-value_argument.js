3a88ad23a6fd841090da231ce358296917e6ff3a
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-04-08 18:34:06 -0700

    javascript

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+// prints the first argument value  passed to it
+
+if (process.argv[2] === undefined) {
+	console.log("NO argument");
+} else {
+	console.log(process.argv[2]);
+}
+
+
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// prints two arguments passed following “ is ”
+
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
