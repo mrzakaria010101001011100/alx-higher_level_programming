321a615645d3aac79193dc8408a845873e3db4ff
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-04-08 22:13:32 -0700

    javascript

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
+//Write a script that prints “JavaScript is amazing”
 
-//Write a script that prints “JavaScript is amazing”:
 const myVar = 'Javascript is amazing';
 console.log(myVar);
