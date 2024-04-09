5202fd28b1a2ffcad09814aad686ada805fd70da
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-04-08 18:48:55 -0700

    javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+// prints 3 lines: (like 1-multi_languages.js) but useing ARRAY.
+// using an array of string and a loop
+
+const array-js = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log( array-js[i]);
+}
