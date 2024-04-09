afbca23448f1136b50fe8db59a405e50c8dccb37
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-04-08 21:47:16 -0700

    javascript

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+// Write a script that prints x times “C is fun”
+// using an array of string and a loop
+
+const x = 'C is fun';
+
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log(x);
+  }
+}
