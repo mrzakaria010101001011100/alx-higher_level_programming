2d769b887e9732802e162ac16d6146c702e454e4
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-04-11 09:29:55 -0700

    javascript

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
+
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
 myObject.incr = function () {
-  this.value++;
+  myObject.value++;
 };
 myObject.incr();
 console.log(myObject);
