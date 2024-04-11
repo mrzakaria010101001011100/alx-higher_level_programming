d18e4083f9baa44ad434996b3df6fafd0027c7d8
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-04-11 09:15:43 -0700

    javascript

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-
 if (process.argv.length <= 3) {
-  console.log(0);
+  console.log('0');
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
+//Write a script that searches the second biggest integer in the list of arguments.
