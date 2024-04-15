2a31a384eb8697b02409064c7bd0b23469382150
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-04-14 17:14:38 -0700

    javascript

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+'''Script that lists all states from the database hbtn_0e_0_usa'''
 import MySQLdb
 from sys import argv
-
-# The code should not be executed when imported
+'''The code should not be executed when imported'''
 if __name__ == '__main__':
 
     # make a connection to the database
